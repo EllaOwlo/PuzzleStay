f9b3c37fbc6926a04aee7246570275ecd79c6473
yembel <yembel84@hotmail.com>
2025-05-17 22:50:59 +0100

    updates html

--- root/js/app.js
+++ root/js/app.js
@@ -1,25 +1,15 @@
 
 
   
-//   $(document).ready(function () {
-//     $(".nav-btn").on("click", function () {
-//       const target = $(this).data("target");
+  $(document).ready(function () {
+    $(".nav-btn").on("click", function () {
+      const target = $(this).data("target");
       
-//       // Hide all sections and show the selected one
-//       $(".section").not(target).hide(); // hide all
-//       $(target).fadeIn(500); // show the one clicked
-//     });
-//     });
+      // Hide all sections and show the selected one
+      $(".section").not(target).hide(); // hide all
+      $(target).fadeIn(500); // show the one clicked
+    });
+    });
   
     
 
-// Global variables
-const defaultSection = "#homebig";
-
-// Reusable function to show only the selected section
-function showOnlySection(target) {
-  $(".section").hide();           // Hide all
-  $(target).fadeIn(500);          // Show selected with fade
-}
-
-  
--- root/js/main.js
+++ root/js/main.js
@@ -1,10 +1,23 @@
+alert("dance")
+
+
 $(document).ready(function () {
-  //  Navigation button click using data-target
+  //  Navigation button for homepage
   $(".nav-btn").on("click", function () {
     const target = $(this).data("target"); // e.g. "#how-to-play"
 
-    $(".section").not(target).hide(); // hide all except the selected one
-    $(target).fadeIn(500);            // show the target with fade
+    $(".section").not("#how-to-play").hide();// hide all except the selected one
+    $("#how-to-play").fadeIn(500);            // show the target with fade
   });
 
 })
+
+
+$(document).ready(function () {
+    $(".nav-btn").on("click", function () {
+      const target = $(this).data("target"); // gets "#map"
+      $(".section").not(target).hide();
+      $(target).fadeIn(500); // shows the map section
+    });
+  });
+  
